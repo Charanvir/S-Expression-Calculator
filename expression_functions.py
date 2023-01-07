f6fb1e9e1501e95cc33a8ffef4582a9aa79c5ca7
Charanvir <charanvir123@gmail.com>
2023-01-07 03:06:21 -0500

    added comments explaining the functionality of the evaluate function

--- expression_functions.py
+++ expression_functions.py
@@ -1,3 +1,6 @@
+# The evaluate function will work as intended when there are integer gives for each expression. It will be called
+# multiple times by another function, which breaks apart the user input into separate expressions,
+# such as nested expression
 def evaluate(expression):
     parts_of_expression = expression.split()
     operator = parts_of_expression[0]
@@ -8,5 +11,3 @@ def evaluate(expression):
     elif operator == 'multiply':
         return int(exp1) * int(exp2)
     return int(expression)
-
-
