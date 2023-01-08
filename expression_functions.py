5f0ff9aaf96fba516268b18956fab758e8b01b12
Charanvir <charanvir123@gmail.com>
2023-01-07 20:45:42 -0500

    updated functions in expression_functions.py to allow for 2 or more operands per expression, instead of jus calculating 2

--- expression_functions.py
+++ expression_functions.py
@@ -1,3 +1,6 @@
+error_message = "Please enter the expression in the correct format, with at least two valid operands per expression)"
+
+
 def output_function(user_input):
     try:
         while ')' in user_input:
@@ -6,24 +9,29 @@ def output_function(user_input):
             try:
                 evaluated_expression = evaluate(user_input[left_index + 1:right_index])
             except IndexError:
-                return "Please ensure that each expression has at least two valid operands"
+                return error_message
             user_input = user_input[:left_index] + str(evaluated_expression) + user_input[right_index + 1:]
         return int(user_input)
     except ValueError:
-        return "Please enter the expression in the correct format."
+        return error_message
 
 
 def evaluate(expression):
     parts_of_expression = expression.split()
+    if len(parts_of_expression) <=2:
+        return error_message
     operator = parts_of_expression[0]
-    exp1 = parts_of_expression[1]
-    exp2 = parts_of_expression[2]
-    if operator == 'add':
-        return int(exp1) + int(exp2)
-    elif operator == 'multiply':
-        return int(exp1) * int(exp2)
+    exp1 = int(parts_of_expression[1])
+    if operator == "add":
+        for operand in parts_of_expression[2:]:
+            exp1 = exp1 + int(operand)
+    elif operator == "multiply":
+        for operand in parts_of_expression[2:]:
+            exp1 = exp1 * int(operand)
     elif operator == "exponent":
-        return int(exp1) ** int(exp2)
+        for operand in parts_of_expression[2:]:
+            exp1 = exp1 ** int(operand)
     elif operator == "subtract":
-        return int(exp1) - int(exp2)
-    return int(expression)
+        for operand in parts_of_expression[2:]:
+            exp1 = exp1 - int(operand)
+    return int(exp1)
--- test_output.py
+++ test_output.py
@@ -1,5 +1,4 @@
 from expression_functions import output_function
-import pytest
 
 correct_test_cases = [
     ("123", 123),
@@ -8,16 +7,20 @@ correct_test_cases = [
     ("(add (add 3 4) 7)", 14),
     ("(multiply (multiply 3 3) (add 4 9))", 117),
     ("(add (multiply (add 4 6) (multiply 10 5)) (add 5 5))", 510),
-    ("(add (multiply (add 5 5) 5) (add 3 4))", 57)
+    ("(add (multiply (add 5 5) 5) (add 3 4))", 57),
+    ("(add (multiply 2 4 6) (add 2 4 6) (multiply 3 3 3))", 87),
+    ("(multiply (add (add 1 2 3) 5) (exponent 2 2 2) (subtract 6 4))", 352)
 ]
 
+error_message = "Please enter the expression in the correct format, with at least two valid operands per expression)"
 error_handling_test_cases = [
-    ("abc", "Please enter the expression in the correct format."),
-    ("add 1 1", "Please enter the expression in the correct format."),
-    ("(add 1 (add 1 1)", "Please enter the expression in the correct format."),
-    ("(add 1)", "Please ensure that each expression has at least two operands"),
-    ("(add (add 1 1))", "Please ensure that each expression has at least two operands"),
-    ("(multiply (add 1 1) (multiply 1)", "Please ensure that each expression has at least two operands")
+    ("abc", error_message),
+    ("add 1 1", error_message),
+    ("(add 1 (add 1 1)", error_message),
+    ("(add 1)", error_message),
+    ("(add (add 1 1))", error_message),
+    ("(multiply (add 1 1) (multiply 1)", error_message),
+    ("(add (multiply 1)", error_message)
 ]
 
 
@@ -29,5 +32,5 @@ def test_correct_output():
 
 def test_potential_error():
     for test in error_handling_test_cases:
-        expression, error_message = test
-        assert output_function(expression) == error_message
+        expression, message = test
+        assert output_function(expression) == message
